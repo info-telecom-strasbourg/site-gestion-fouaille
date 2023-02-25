c04aefc0f1c30f1312e60c8c0b1aaab72a9bcfbb
ebergamini <bergaminienzo62@gmail.com>
2023-02-25 22:22:40 +0100

    traitement des données pour le graph (en cours)

--- graph.php
+++ graph.php
@@ -4,26 +4,46 @@
     $value = 'value="';
     $boutton = '';
     $graph = '';
+    $tab_produit = '';
+    $tab_graph = '';
+    $donnees_json = '';
+    $tab = '';
 
     if(!empty($_GET["date"]) ){
         $date_debut = $_GET["date"] . ' 17:00:00';
-        $date_fin = $_GET["date"] . ' 23:30:00';
+        $date_fin = $_GET["date"] . ' 23:00:00';
         $value = $value . $_GET["date"] . '"';
 
         $boutton = '<button type="submit" class="btn btn-primary">Envoyer</button>';
 
         $graph = '<div id="chart_div" style="width: 100%; height: 600px;"></div>';
 
-        $sql_produit = 'SELECT DISTINCT produit FROM Commande WHERE date_histo > "'. $date_debut . '" AND date_histo < "' . $date_fin . '" AND delta < 0';
+        $start_date = DateTime::createFromFormat('Y-m-d H:i:s', $_GET["date"]. ' 22:00:00');
+        $end_date = DateTime::createFromFormat('Y-m-d H:i:s', $date_fin);
 
-        if ($pdo->query($sql_produit) === false) {
-            var_dump($pdo->errorInfo());
-        } else {
-            $tab_produit = $pdo->query($sql_produit)->fetchAll(PDO::FETCH_COLUMN, 0);
+        $current_date = $start_date;
+
+        $tab = array();
+
+        while ($current_date <= $end_date) { // Création du tableau des dates pour le graphique
+            $sql_graph = 'SELECT produit, -SUM(delta) AS recette FROM Commande WHERE date_histo > "'. $date_debut . '" AND date_histo < "' . $current_date->format('Y-m-d H:i:s') . '" AND delta < 0 GROUP BY produit';
+
+            if ($pdo->query($sql_graph) === false){
+                var_dump($pdo->errorInfo());
+            } else {
+                $tab_graph = $pdo->query($sql_graph)->fetchAll(PDO::FETCH_ASSOC);
+            }
+
+            $tab['' . $current_date->format('Y-m-d H:i:s') . ''] = $tab_graph;
+            $current_date->add(new DateInterval('PT15M'));
         }
+
+        $donnees_json = json_encode($tab);
 
     }else{
         $value = '';
+
+        $graph = '<div id="chart_div" style="width: 100%; height: 600px;"></div>';
 
         $boutton = '<button type="submit" class="btn btn-secondary">Envoyer</button>';
     }
@@ -45,23 +65,39 @@
         google.charts.load('current', {'packages':['corechart']});
         google.charts.setOnLoadCallback(drawChart);
 
+
+
         function drawChart() {
-            var data = google.visualization.arrayToDataTable([
-                ['Temps', 'biere12','biere16'],
-                ['10',  3, 10],
-                ['20',  6, 15],
-                ['30',  9, 20],
-                ['40',  12, 25],
-                ['50',  15, 30],
-                ['60',  18, 35],
-                ['70',  21, 40],
-                ['80',  24, 45],
-                ['90',  27, 50]
-            ]);
+            // Création d'une table de données
+            var data = new google.visualization.DataTable();
+
+            data.addColumn('string', 'Temps');
+            data.addColumn('string', 'Produit');
+            data.addColumn('number', 'Recette');
+
+            var donnees_js = JSON.parse('<?php echo $donnees_json; ?>');
+
+            console.log(donnees_js);
+
+            for (var key in donnees_js){
+
+                var heure = key.split(" ")[1];
+
+
+                for (var i = 0; i < donnees_js[key].length; i++) {
+                    var produit = donnees_js[key][i].produit;
+                    var recette = parseFloat(donnees_js[key][i].recette);
+
+                    data.addRow([heure, produit, recette]);
+
+                    console.log(data);
+                }
+            }
+
 
             var options = {
-                title: 'Recette par minute',
-                hAxis: {title: 'Date',  titleTextStyle: {color: '#333'}},
+                title: 'Recette en fonction du temps',
+                hAxis: {title: 'Temps',  titleTextStyle: {color: '#333'}},
                 vAxis: {minValue: 0}
             };
 
@@ -90,7 +126,8 @@
             <?php echo $graph; ?>
             <pre>
                 <?php
-                    var_dump($tab_produit);
+                    var_dump($tab);
+                    var_dump($donnees_json);
                 ?>
             </pre>
         </div>
