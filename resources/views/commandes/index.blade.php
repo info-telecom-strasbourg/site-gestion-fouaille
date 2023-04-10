46cd2bb14b8a87689cc089617af10fec5ab1f8ec
ebergamini <bergaminienzo62@gmail.com>
2023-04-10 14:56:28 +0200

    Add index commande page
    remove views and add new layout and a comment page

--- resources/views/commandes/index.blade.php
+++ resources/views/commandes/index.blade.php
@@ -1,3 +1,26 @@
-@foreach($commandes as $commande)
-    <p>{{ $commande->member->last_name }}</p>
-@endforeach
+<x-layout>
+    <div class="container">
+        <table class="table">
+            <thead>
+            <tr>
+                <th scope="col">id</th>
+                <th scope="col">Nom</th>
+                <th scope="col">Produit</th>
+                <th scope="col">Prix</th>
+                <th scope="col">Quantité</th>
+            </tr>
+            </thead>
+            <tbody>
+            @foreach($commandes as $commande)
+                <tr>
+                    <th scope="row">{{ $commande->id }}</th>
+                    <td>{{ $commande->member->nickname }}</td>
+                    <td>{{ $commande->products->name }}</td>
+                    <td>{{ $commande->price }}</td>
+                    <td>{{ $commande->amount }}</td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+    </div>
+</x-layout>
---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="/docs/4.0/assets/img/favicons/favicon.ico">
+
+    <title>Fixed top navbar example for Bootstrap</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/navbar-fixed/">
+
+    <!-- Bootstrap core CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+
+    <!-- Custom styles for this template -->
+    <link href="navbar-top-fixed.css" rel="stylesheet">
+</head>
+
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button
+            class="navbar-toggler"
+            type="button"
+            data-mdb-toggle="collapse"
+            data-mdb-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+        >
+            <i class="fas fa-bars"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                <a class="nav-link" href="#">Features</a>
+                <a class="nav-link" href="#">Pricing</a>
+            </div>
+        </div>
+    </div>
+</nav>
+
+{{ $slot}}
+
+</body>
+</html>
