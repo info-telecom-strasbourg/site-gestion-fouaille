1b2bfffa4acbd6ed03dc235482344adad145c62a
EnzoBergamini <115539037+EnzoBergamini@users.noreply.github.com>
2023-06-07 13:48:14 +0200

    add api requeste to get organization logo
    add intervention image to composer and a way to resize the image in the
    request

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,8 @@ use App\Http\Controllers\Api\ApiOrganizationController;
 use App\Http\Controllers\Api\ApiProductController;
 use Illuminate\Support\Facades\Route;
 
+use Illuminate\Http\Request;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -19,9 +21,14 @@ Route::prefix('product')->group( function() {
     Route::get('/', [ApiProductController::class, 'index']);
 });
 
+Route::get('/', function (Request $request) {
+    dd($request->all());
+});
+
 Route::prefix('organization')->group( function() {
     Route::get('index', [ApiOrganizationController::class, 'index']);
     Route::get('index/small', [ApiOrganizationController::class, 'indexSmall']);
 
     Route::get('show/{id}', [ApiOrganizationController::class, 'show']);
+    Route::get('show/{id}/image', [ApiOrganizationController::class, 'image']);
 });
