00cb0a7f4aa28af32e6f41f39d661250c0ff380a
EnzoBergamini <115539037+EnzoBergamini@users.noreply.github.com>
2023-08-11 17:50:50 +0200

    preparation of challenges page

--- resources/views/challenge/index.blade.php
+++ resources/views/challenge/index.blade.php
@@ -1,5 +1,40 @@
 @section('title', 'Défis de l\'intégration')
 
 <x-layout>
-    <h1>Intégration</h1>
+    <div class="row">
+
+        <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card border-left-primary shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                Participants</div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">30</div>
+                        </div>
+                        <div class="col-auto">
+                            <i class="fas fa-user-check fa-2x text-gray-300"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card border-left-success shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
+                                Défis réalisé</div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">130</div>
+                        </div>
+                        <div class="col-auto">
+                            <i class="fas fa-trophy fa-2x text-gray-300"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </x-layout>
