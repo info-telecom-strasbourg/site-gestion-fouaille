c674593b73c761862667ab84af36850cea2651b9
EnzoBergamini <115539037+EnzoBergamini@users.noreply.github.com>
2023-08-11 11:48:48 +0200

    Handle if there is no data

--- resources/views/asso/index.blade.php
+++ resources/views/asso/index.blade.php
@@ -1,43 +1,49 @@
 @section('title', 'Asso/Club')
 
 <x-layout>
-    <div class="card shadow mb-4">
-        <div class="card-header py-3">
-            <h6 class="m-0 font-weight-bold text-primary">Associations / Clubs</h6>
+    @if(empty($data))
+        <div class="alert alert-danger" role="alert">
+            Aucun membre n'a été trouvé.
         </div>
-        <div class="card-body">
-            <div class="table-responsive">
-                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-                    <thead>
-                    @foreach($data[0] as $key => $value)
-                        @if($key != 'Id')
-                            <th>{{ $key }}</th>
-                        @endif
-                    @endforeach
-                    </thead>
-                    <tbody>
-                    @foreach($data as $key => $value)
-                        <tr>
-                            @foreach($value as $key2 => $value2)
-                                @if($key2 != 'Id')
-                                    @if($key2 == 'Logo')
-                                        <td><img src="{{ $value2 }}" alt="Logo" width="100" height="100"></td>
-                                    @elseif($key2 == 'Nom')
-                                        <td><a href="{{ route('asso.show', $value['Id']) }}">{{ $value2 }}</a></td>
-                                    @elseif(filter_var($value2, FILTER_VALIDATE_URL))
-                                        <td><a href="{{ $value2 }}">{{ $value2 }}</a></td>
-                                    @elseif(filter_var($value2, FILTER_VALIDATE_EMAIL))
-                                        <td><a href="mailto:{{ $value2 }}">{{ $value2 }}</a></td>
-                                    @else
-                                        <td>{{ $value2 }}</td>
+    @else
+        <div class="card shadow mb-4">
+            <div class="card-header py-3">
+                <h6 class="m-0 font-weight-bold text-primary">Associations / Clubs</h6>
+            </div>
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                        <thead>
+                        @foreach($data[0] as $key => $value)
+                            @if($key != 'Id')
+                                <th>{{ $key }}</th>
+                            @endif
+                        @endforeach
+                        </thead>
+                        <tbody>
+                        @foreach($data as $key => $value)
+                            <tr>
+                                @foreach($value as $key2 => $value2)
+                                    @if($key2 != 'Id')
+                                        @if($key2 == 'Logo')
+                                            <td><img src="{{ $value2 }}" alt="Logo" width="100" height="100"></td>
+                                        @elseif($key2 == 'Nom')
+                                            <td><a href="{{ route('asso.show', $value['Id']) }}">{{ $value2 }}</a></td>
+                                        @elseif(filter_var($value2, FILTER_VALIDATE_URL))
+                                            <td><a href="{{ $value2 }}">{{ $value2 }}</a></td>
+                                        @elseif(filter_var($value2, FILTER_VALIDATE_EMAIL))
+                                            <td><a href="mailto:{{ $value2 }}">{{ $value2 }}</a></td>
+                                        @else
+                                            <td>{{ $value2 }}</td>
+                                        @endif
                                     @endif
-                                @endif
-                            @endforeach
-                        </tr>
-                    @endforeach
-                    </tbody>
-                </table>
+                                @endforeach
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
-    </div>
+    @endif
 </x-layout>
--- resources/views/asso/show.blade.php
+++ resources/views/asso/show.blade.php
@@ -1,35 +1,41 @@
 @section('title', 'Asso/Club')
 
 <x-layout>
-    <div class="card shadow mb-4">
-        <div class="card-header py-3">
-            <h6 class="m-0 font-weight-bold text-primary">{{ $data['Nom'] }}</h6>
+    @if(empty($data))
+        <div class="alert alert-danger" role="alert">
+            Aucun membre n'a été trouvé.
         </div>
-        <div class="card-body">
-            <div class="table-responsive">
-                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-                    <thead>
-                    @foreach($data as $key => $value)
-                        <th>{{ $key }}</th>
-                    @endforeach
-                    </thead>
-                    <tbody>
-                        <tr>
-                            @foreach($data as $key => $value)
-                                @if($key == 'Logo')
-                                    <td><img src="{{ $value }}" alt="Logo" width="100" height="100"></td>
-                                @elseif(filter_var($value, FILTER_VALIDATE_URL))
-                                    <td><a href="{{ $value }}">{{ $value }}</a></td>
-                                @elseif(filter_var($value, FILTER_VALIDATE_EMAIL))
-                                    <td><a href="mailto:{{ $value }}">{{ $value }}</a></td>
-                                @else
-                                    <td>{{ $value }}</td>
-                                @endif
-                            @endforeach
-                        </tr>
-                    </tbody>
-                </table>
+    @else
+        <div class="card shadow mb-4">
+            <div class="card-header py-3">
+                <h6 class="m-0 font-weight-bold text-primary">{{ $data['Nom'] }}</h6>
+            </div>
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                        <thead>
+                        @foreach($data as $key => $value)
+                            <th>{{ $key }}</th>
+                        @endforeach
+                        </thead>
+                        <tbody>
+                            <tr>
+                                @foreach($data as $key => $value)
+                                    @if($key == 'Logo')
+                                        <td><img src="{{ $value }}" alt="Logo" width="100" height="100"></td>
+                                    @elseif(filter_var($value, FILTER_VALIDATE_URL))
+                                        <td><a href="{{ $value }}">{{ $value }}</a></td>
+                                    @elseif(filter_var($value, FILTER_VALIDATE_EMAIL))
+                                        <td><a href="mailto:{{ $value }}">{{ $value }}</a></td>
+                                    @else
+                                        <td>{{ $value }}</td>
+                                    @endif
+                                @endforeach
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
-    </div>
+    @endif
 </x-layout>
