8bfe8a89ed2660b581dd3e14e2feb10d6dc1a0ef
EnzoBergamini <115539037+EnzoBergamini@users.noreply.github.com>
2023-11-02 11:20:20 +0100

    Ass dynamic periode

--- app/Http/Controllers/FouailleChartsController.php
+++ app/Http/Controllers/FouailleChartsController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers;
 use App\Models\Order;
 
 use App\Charts\OrderLineGraph;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use function PHPUnit\Framework\isEmpty;
 
 class FouailleChartsController extends Controller
 {
@@ -25,14 +27,18 @@ class FouailleChartsController extends Controller
 
         $products = $data->pluck('product_name')->unique();
 
+        // take the période between start_at and end_at and split it in (end_at - start_at / 35) intervals and use carbon to do it
+
         $time = [];
-        $start_at = new \DateTime($start_at);
-        $end_at = new \DateTime($end_at);
-        $interval = new \DateInterval('PT15M');
-        $period = new \DatePeriod($start_at, $interval, $end_at);
-        foreach ($period as $dt) {
-            $time[] = $dt->format('Y-m-d H:i:s');
+        $start_at = new Carbon($start_at);
+        $end_at = new Carbon($end_at);
+        $diff = $end_at->diffInMinutes($start_at);
+        $interval = $diff / 30;
+        for ($i = 0; $i < 30; $i++) {
+            $time[$i] = $start_at->addMinutes($interval)->format('Y-m-d H:i:s');
         }
+
+        $data_sets = [];
 
         foreach ($products as $product) {
             for ($i = 0; $i < count($time) - 1; $i++) {
@@ -42,17 +48,23 @@ class FouailleChartsController extends Controller
             $data_sets[$product]['color'] = $data->where('product_name', $product)->pluck('color')->first();
         }
 
-
         $chart = new OrderLineGraph;
+
+        //take $time without the last element
+        array_pop($time);
 
         $chart->labels($time);
         foreach ($products as $product) {
             $chart->dataset($product, 'line', $data_sets[$product]['amount'])
-                ->color($data_sets[$product]['color'])->fill(false);
+                ->color($data_sets[$product]['color'])
+                ->fill(false);
         }
 
-
-
-        return view('fouaille.chart.index', compact('chart'));
+        return view('fouaille.chart.index', [
+            'chart' => $chart,
+            'is_data' => count($data_sets) > 0 ? true : false,
+            'start_at' => $start_at->format('Y-m-d H:i:s'),
+            'end_at' => $end_at->format('Y-m-d H:i:s')
+        ]);
     }
 }
